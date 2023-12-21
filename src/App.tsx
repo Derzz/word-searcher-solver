4d9e27f0b0630c958b51c8e033f66bd31e0e7211
Michael Xie <michaelx.10@hotmail.com>
2023-12-21 14:56:58 -0500

    Added (temporary?) class component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import logo from './logo.svg';
+import solver from './components/solver'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        phrase: ""
+    }
+
+    setPhrase = (value: string) => {
+        this.setState({phrase: value});
+    }
+
+    constructor(props: any) {
+        super(props);
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <textarea
+                        name="Text1"
+                        cols={40}
+                        value={this.state.phrase}
+                        onChange={e => this.setPhrase(e.target.value)}
+                        rows={5}/>
+                    <button id={'start'} type={'button'}>Click me!</button>
+                </header>
+            </div>
+        );
+    }
 }
+
 
 export default App;
---
+++ src/components/solver.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const solver = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default solver;
