5fb4c5122e14a068f445edba987a98bc1b64d54c
Michael Xie <michaelx.10@hotmail.com>
2023-12-21 15:56:33 -0500

    Michael Test

--- src/components/solver.ts
+++ src/components/solver.ts
@@ -1,8 +1,7 @@
 const solver = function (gridString: string, wordString: string) {
     let grid: Array<Array<String>>;
     let words: Array<String>;
-    console.log("clicked!");
-    console.log(gridString);
+    console.log("michael!");
 
     //TODO Steps
     // 1. Split gridString into grid, separate components as needed
