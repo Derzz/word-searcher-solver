520b0c9b06aaa161ca0867bbe15839686bf03c86
Printhell0w0rld <shihhsi0427@gmail.com>
2023-12-23 19:23:42 -0500

    add preview block

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, {Component, useState} from 'react';
-import logo from './logo.svg';
 import solver from './components/solver'
 import './App.css';
+
+
+
+
 
 
 class App extends Component {
     state = {
         grid: "",
-        words: ""
+        words: "",
+        preview: ['']
     }
 
     setGrid = (value: string) => {
@@ -18,17 +22,37 @@ class App extends Component {
         this.setState({words: value});
     }
 
-
     constructor(props: any) {
         super(props);
+        //this.state = { preview: [''] };
     };
 
+    Handleclick = () => {
+        //const [splitted,setSplitted]= useState();
+        console.log(10);
+        var gridstring=(document.getElementById("grid") as HTMLInputElement).value;
+        //console.log();
+        //setSplitted(gridstring.split("\n")); 
+        //console.log(gridstring.split("\n")); 
+        console.log(gridstring);  
+        
+        this.setState({ preview: gridstring.split("\n")});  
+        //this.setState({ preview: gridstring});  
+        
+    }
+
     render() {
+        //var splitted=[''];
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <textarea
+                <header className="App-header "> //App-header 
+                    <h1 className="text-black-700 text-3xl font-bold">
+                        Word Solver
+                    </h1>
+                    <div className="hello">
+                    <textarea 
+                        id="grid"
+                        className=" w-3/12 ml-2 bg-slate-100 border-solid border-2 bg-right resize-none" 
                         name="Word Searcher Puzzle"
                         cols={40}
                         value={this.state.grid}
@@ -36,15 +60,38 @@ class App extends Component {
                         rows={5}/>
 
                     <textarea
+                        className=" w-3/12 ml-2 bg-slate-100 border-solid border-2 bg-right resize-none"
                         name="Words"
                         cols={40}
                         value={this.state.words}
                         onChange={e => this.setWords(e.target.value)}
                         rows={5}/>
+                    </div>
 
-                    <button id={"start"} type={"button"} onClick={() => solver(this.state.grid, this.state.words)}>Click
-                        me!
+                    
+                    
+
+                    <button 
+                        className="bg-slate-300 rounded w-20" id={"start"} type={"button"} onClick={() => solver(this.state.grid, this.state.words)}>
+                        Click me !
                     </button>
+                    
+                    <button onClick={this.Handleclick}>
+                            hi
+                        </button>
+
+                    <div className="preview">
+                    
+                    {this.state.preview.map(show => (
+                        <p className='demo'> {show.split('').map(show2 => (
+                            <p> {show2} </p>
+                            
+                        ))} </p>
+                        
+                    ))}
+
+                    </div>
+                    
                 </header>
             </div>
         );
