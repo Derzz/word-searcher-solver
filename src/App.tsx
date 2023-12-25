984ca316a1867351378ba31142daf857bda49793
Michael Xie <michaelx.10@hotmail.com>
2023-12-25 18:14:27 -0500

    solved some logic issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React, {Component, useState} from 'react';
-import solver from './components/solver'
+import setUp from './components/solver'
 import './App.css';
-
-
-
-
 
 
 class App extends Component {
     state = {
-        grid: "",
-        words: "",
         preview: [''],
         grid: "ATJAA\nABOBA\nAAEMA",
         words: "BOB\nJOE\nTOM",
@@ -54,42 +48,43 @@ class App extends Component {
     Handleclick = () => {
         //const [splitted,setSplitted]= useState();
         console.log(10);
-        var gridstring=(document.getElementById("grid") as HTMLInputElement).value;
+        var gridstring = (document.getElementById("grid") as HTMLInputElement).value;
         //console.log();
         //setSplitted(gridstring.split("\n")); 
         //console.log(gridstring.split("\n")); 
-        console.log(gridstring);  
-        
-        this.setState({ preview: gridstring.split("\n")});  
+        console.log(gridstring);
+
+        this.setState({preview: gridstring.split("\n")});
         //this.setState({ preview: gridstring});  
-        
+
     }
 
     render() {
         //var splitted=[''];
         return (
             <div className="App">
-                <header className="App-header "> 
+                <header className="App-header ">
                     <h1 className="title">
                         Word Solver
                     </h1>
                     <div className="bigblock">
                         <div className="hello">
-                            
-                            <div className=" h-8 pt-1 w-80 rounded-md text-white bg-cyan-600 text-center align-middle"> 
+
+                            <div className=" h-8 pt-1 w-80 rounded-md text-white bg-cyan-600 text-center align-middle">
                                 Create board
                             </div>
 
-                            <textarea 
+                            <textarea
                                 id="grid"
-                                className=" w-80 h-64 bg-slate-100 border-solid border-2 bg-right resize-none rounded" 
+                                className=" w-80 h-64 bg-slate-100 border-solid border-2 bg-right resize-none rounded"
                                 name="Word Searcher Puzzle"
                                 cols={40}
                                 value={this.state.grid}
                                 onChange={e => this.setGrid(e.target.value)}
                                 rows={5}/>
 
-                            <div className=" h-8 pt-1 w-80 rounded-md text-white bg-cyan-600 text-center align-middle mt-4"> 
+                            <div
+                                className=" h-8 pt-1 w-80 rounded-md text-white bg-cyan-600 text-center align-middle mt-4">
                                 Find word
                             </div>
                             <textarea
@@ -101,15 +96,16 @@ class App extends Component {
                                 onChange={e => this.setWords(e.target.value)}
                                 rows={5}/>
 
-                            <button 
-                                className="button-example" 
-                                id={"start"} type={"button"} onClick={() => solver(this.state.grid, this.state.words)}>
+                            <button
+                                className="button-example"
+                                id={"start"} type={"button"}
+                                onClick={() => this.checkGrid(this.state.grid, this.state.words)}>
                                 click me
                             </button>
                         </div>
 
                         <div>
-                            <div className="preview-bar"> 
+                            <div className="preview-bar">
                                 Preview
                             </div>
 
@@ -117,30 +113,26 @@ class App extends Component {
                                 <div className="preview">
                                     {this.state.preview.map(show => (
                                         <p className='demo'> {show.split('').map(show2 => (
-                                            
+
                                             <p> {show2} </p>
 
                                         ))} </p>
-                                        
+
                                     ))}
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
 
-                    
-                    
+
                     <button onClick={this.Handleclick}>
-                            hi
+                        hi
                     </button>
 
-                    
-                    
-                    
+
                     <button id={"start"} type={"button"}
                             onClick={() => this.checkGrid(this.state.grid, this.state.words)}>
-                                Click me!
+                        Click me!
                     </button>
 
                     // TODO Add a way to change font color based on color selected
