2538a2d686f13ef419c452b21f9e12bbe221e51a
Michael Xie <michaelx.10@hotmail.com>
2023-12-30 14:17:20 -0500

    added navbar component to pages

--- src/Howitworks.tsx
+++ src/Howitworks.tsx
@@ -2,59 +2,12 @@ import React, {Component, useState} from 'react';
 import solver from './components/solver';
 import setUp from './components/solver';
 import './App.css';
-import {Routes,Route,Link} from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
+import Navbar from "./components/navbar";
 
-function Howitworks(){
-    return(
-        
-            
-        <div className="title">
-                <div>
-                    <div className='title-block'>W</div>
-                </div>
-                <div>
-                    <div className='title-block'>O</div>
-                </div>
-                <div>
-                    <div className='title-block'>R</div>
-                </div>
-                <div>
-                    <div className='title-block'>D</div>
-                </div>
-                <div>
-                    <div className='title-block'>S</div>
-                </div>
-                <div>
-                    <div className='title-block'>O</div>
-                </div>
-                <div>
-                    <div className='title-block'>L</div>
-                </div>
-                <div>
-                    <div className='title-block'>V</div>
-                </div>
-                <div>
-                    <div className='title-block'>E</div>
-                </div>
-                <div>
-                    <div className='title-block'>R</div>
-                </div>
-
-                <div className="Nav-bar-block">
-                    <div>
-                        <li className="Nav-bar"> <a href="./home">Home </a> </li>
-                    </div>  
-                    
-                    <div>
-                        <li className="Nav-bar"> <a href="./about">About </a> </li>
-                    </div>
-                    
-                    <div>
-                        <li className="Nav-bar"> <a href="./howitworks">How it works </a> </li>
-                    </div>
-                </div> 
-            </div>
-
+function Howitworks() {
+    return (
+        <Navbar/>
     );
 }
 
